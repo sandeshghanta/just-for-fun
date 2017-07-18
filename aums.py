c6ae7860cc983049610ac4d1b4a798fce1624023
sandeshghanta <sghanta05@gmail.com>
2017-07-18 21:32:10 +0530

    Add files via upload
    A python code which opens the aums of my university in a browser and logs in with the credentials we provide it.

---
+++ aums.py
@@ -0,0 +1,15 @@
+from splinter import Browser
+from selenium import webdriver
+import selenium
+#driver = webdriver.Chrome()
+with Browser('chrome') as browser:
+    browser.visit('https://aums-students-am.amrita.edu:8443/cas/login?service=https%3A%2F%2Faums-students-am.amrita.edu%3A8443%2Faums%2FJsp%2FCore_Common%2Findex.jsp')
+    browser.find_by_id('username').first.value = "AM.EN.U4CSE16254"
+    browser.find_by_id('password').first.value = "sandeshghanta047"
+    browser.find_by_name('submit').click()
+    browser.find_by_id('username').first.value = "AM.EN.U4CSE16254"
+    browser.find_by_id('password').first.value = "sandeshghanta047"
+    browser.find_by_name('submit').click()
+    browser.execute_script("document.getElementById('oM_m701').style.visibility='visible';")
+    #browser.execute_script("document.getElementById('oM_m701').click();")
+    input('Press ENTER to exit')
